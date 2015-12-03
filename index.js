393424a9c9a6f2ac3dc4ba8a2cb842e6c0a04553
Alice Frumin <alifrumin@yahoo.com>
2015-12-03 16:11:55 -0500

    index working

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.get("/", function(req, res){
   res.render("index.html");
 });
 
-app.use("/links", require("./controllers/links"));
-app.use("/topics", require("./controllers/topics"));
+app.use("/links", require("./controllers/linksController"));
+app.use("/topics", require("./controllers/topicsController"));
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
