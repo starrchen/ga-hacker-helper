aaf6ea5c3ced0654b65daa613f55c517f437345b
Alexandra <akscreven@gmail.com>
2015-12-04 14:26:35 -0500

    added schema for links and topics

---
+++ db/schema.js
@@ -0,0 +1,28 @@
+// requiring mongoose dependency
+var mongoose = require('mongoose');
+
+// instantiate a name space for our Schema constructor defined by mongoose.
+var Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+// defining schema for reminders
+var LinkSchema = new Schema({
+  title: String,
+  summary: String,
+  url: String,
+  source: String,
+  topics: [TopicSchema]
+  // topic_id: foreign_key
+});
+
+// defining schema for authors.
+var TopicSchema = new Schema({
+  name: String,
+  description: String,
+  image_url: String,
+  links: [LinkSchema]
+});
+
+// setting models in mongoose utilizing schemas defined above
+var LinkModel = mongoose.model("Link", LinkSchema);
+var TopicModel = mongoose.model("Topic", TopicSchema);
