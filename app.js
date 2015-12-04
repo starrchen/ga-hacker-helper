b373aaa9a1b2cea2b150db637a0851f97842b972
Alexandra <akscreven@gmail.com>
2015-12-04 13:59:56 -0500

    index and layout views are linked

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
+var hbs = require('hbs');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
 app.listen(4000, function(){
  console.log("app listening on port 4000");
 });
 
 app.get("/", function(req, res){
-  res.send("Hello World");
+  res.render('index');
 });
