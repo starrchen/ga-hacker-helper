7e934c580f1efebf7168c29ee542322d888f5300
Alice Frumin <alifrumin@yahoo.com>
2015-12-04 16:19:51 -0500

    just in case

--- controllers/topicsController.js
+++ controllers/topicsController.js
@@ -2,36 +2,41 @@ var TopicModel = require("../models/topic");
 var LinkModel = require("../models/link");
 
 var topicsController = {
-  index: function(req, res){
-    TopicModel.find({}, function(err, docs){
-      res.render("topics/index", {topics: docs});
+  index: function(req, res) {
+    TopicModel.find({}, function(err, docs) {
+      res.render("topics/index", {
+        topics: docs
+      });
     });
   },
-  new: function(req, res){
-      res.render("topics/new");
-    },
-  create: function(req, res){
-    var topic = new TopicModel({name: req.body.name,
-    description: req.body.description,
-    imageUrl: req.body.imageUrl
-  });
-    topic.save(function(err){
-      if (!err){
+  new: function(req, res) {
+    res.render("topics/new");
+  },
+  create: function(req, res) {
+    var topic = new TopicModel({
+      name: req.body.name,
+      description: req.body.description,
+      imageUrl: req.body.imageUrl
+    });
+    topic.save(function(err) {
+      if (!err) {
         res.redirect("topics");
       }
     });
   },
-  show: function(req, res){
+  show: function(req, res) {
     var id = req.params.id;
-    TopicModel.findById(id, function(err, doc){
-      res.render("topics/show", {topic: doc});
+    TopicModel.findById(id, function(err, doc) {
+      res.render("topics/show", {
+        topic: doc
+      });
     });
   },
-  delete: function(req, res){
-  var id = req.params.id;
-  TopicModel.findByIdAndRemove(id, function(err, doc){
-    res.redirect("/topics");
-  });
-}
+  delete: function(req, res) {
+    var id = req.params.id;
+    TopicModel.findByIdAndRemove(id, function(err, doc) {
+      res.redirect("/topics");
+    });
+  }
 };
 module.exports = topicsController;
