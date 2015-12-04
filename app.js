16b2ff40c5b533bff07d50b741915d99b74f5546
Alexandra <akscreven@gmail.com>
2015-12-04 14:17:07 -0500

    loaded the rest of our dependencies

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/project_3_db');
+// loads dependency for middleware for paramters
+var bodyParser = require('body-parser');
+// loads dependency that allows put and delete where not supported in html
+var methodOverride = require('method-override');
 var hbs = require('hbs');
+// sets view engine to handlebars
 app.set('view engine', 'hbs');
+// connects assets like stylesheets
 app.use(express.static(__dirname + '/public'));
 
 app.listen(4000, function(){
@@ -9,5 +17,25 @@ app.listen(4000, function(){
 });
 
 app.get("/", function(req, res){
-  res.render('index');
+  res.render('topics/index');
 });
+
+
+// loads module containing all authors contrller actions. not defined yet...
+// var authorsController = require("./controllers/authorsController")
+// connect mongoose interfaces to reminders mongo db
+
+
+
+
+// allows for parameters in JSON and html
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+// allows for put/delete request in html form
+app.use(methodOverride('_method'));
+
+
+
+
+// first route we'll define together ...
+// app.get("/authors", authorsController.index)
