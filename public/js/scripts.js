e9fc70a63748c81ec57ac80e9381ed72939ba813
Alice Frumin <alifrumin@yahoo.com>
2015-12-08 16:37:07 -0500

    making ajax request

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
-  $("h3").on("click", function() {
+  $(".postlinks").on("click", function() {
     console.log("clicked")
+    $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: "http://localhost:4000/topics"
+    }).done(function(response) {
+      console.log(response);
+    }).fail(function(response){
+      console.log("Ajax get request failed.");
+    })
   })
 })
