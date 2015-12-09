7d5b3c6cceb2aeb63f25011e9ae64c3d07b218e0
Alexandra <akscreven@gmail.com>
2015-12-09 13:54:22 -0500

    adds id to topic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
   $(".linktitle").on("click", function() {
     console.log("clicked");
+    console.log(this.id)
+    
     $.ajax({
       type: 'GET',
       dataType: 'json',
       url: "http://localhost:4000/topics"
     }).done(function(response) {
-
       console.log(response.topics[0].name);
+      console.log(response.topics[0]._id)
       var links = function() {
         for(i = 0; i < response.topics[0].links.length; i++) {
-          console.log(response.topics[0].links[i].url)
+          console.log(response.topics[0].links[i].url);
           $('.postlinks').append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
-
         }
-      }
+      };
       $('.postlinks').append(links);
-
-
     }).fail(function(response){
       console.log("Ajax get request failed.");
     });
