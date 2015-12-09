d19e56373903014d60df2bce35375d3a7a50ff60
Starr Z. Chen <chenstarr@gmail.com>
2015-12-09 14:14:46 -0500

    Ajax requests work for topic names

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,25 +1,47 @@
 $(document).ready(function() {
-  $(".linktitle").on("click", function() {
-    console.log("clicked");
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      url: "http://localhost:4000/topics"
-    }).done(function(response) {
+  var url = "http://localhost:4000/topics.json"
+  var request = $.ajax({
+    url: url,
+    type: "GET",
+    dataType: "json"
+  }).done(function(response){
 
-      console.log(response.topics[0].name);
-      var links = function() {
-        for(i = 0; i < response.topics[0].links.length; i++) {
-          console.log(response.topics[0].links[i].url)
-          $('.postlinks').append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
+    var allTopics = [];
 
-        }
-      }
-      $('.postlinks').append(links);
+    for (var i = 0; i < response.topics.length; i++){
+      console.log(response.topics[i].name);
+      $(".all-topics").append("<div class='topic-'" +  i + "><h3>" + response.topics[i].name + "</h3></div>");
+    }
 
+  }).fail(function(response){
+    console.log("Topics fetch fail");
 
-    }).fail(function(response){
-      console.log("Ajax get request failed.");
-    });
-  });
+  }).always(function(response){
+    console.log("Beep boop!");
+  })
 });
+
+  // $(".linktitle").on("click", function() {
+  //   console.log("clicked");
+  //   $.ajax({
+  //     type: 'GET',
+  //     dataType: 'json',
+  //     url: "http://localhost:4000/topics"
+  //   }).done(function(response) {
+  //     console.log(response);
+      // console.log(response.topics[0].name);
+      // var links = function() {
+      //   for(i = 0; i < response.topics[0].links.length; i++) {
+      //     console.log(response.topics[0].links[i].url)
+      //     $('.postlinks').append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
+      //
+      //   }
+      // }
+      // $('.postlinks').append(links);
+//
+//
+//     }).fail(function(response){
+//       console.log("Ajax get request failed.");
+//     });
+//   });
+// });
