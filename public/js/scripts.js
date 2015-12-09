765113b9d8f62993fcbac54d474715ac685eac9c
Alexandra <akscreven@gmail.com>
2015-12-09 16:56:18 -0500

    currented html format for topic links

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -8,29 +8,28 @@ $(document).ready(function() {
   }).done(function(response){
     var allTopics = [];
     for (var i = 0; i < response.topics.length; i++){
-    console.log(response.topics[i].name);
-    $(".all-topics").append("<div class='topic'><h3 class='topicHeader' id='"+i+"'>" + response.topics[i].name + "</h3></div>");
-  }
-  $(".topicHeader").on("click", function() {
-    console.log("clicked");
-    var topicId = $(this).attr('id')
-    console.log(topicId)
-   
+      console.log(response.topics[i].name);
+      $(".all-topics").append("<div class='topic' id='"+i+"'><h3 class='topicHeader'>" + response.topics[i].name + "</h3></div>");
+    }
 
-      var links = function() {
-        var linksArray = []
-        for(i = 0; i < response.topics[topicId].links.length; i++) {
-          console.log(response.topics._id);
-          console.log(response.topics[topicId].links[i].url);
-          linksArray[i] = ("<li><a href='" + response.topics[topicId].links[i].url + "'>" + response.topics[topicId].links[i].url + '</a>');
+    $(".topicHeader").on("click", function() {
+      console.log("clicked");
 
-        }
-        return linksArray
-      };
-      links()
-      $('#'+topicId).append(links);
+      //if links exist
+        // $(this)
+      var topicId = $(this).parent().attr('id')
+      console.log(topicId)
+      var linksArray = []
+      for(i = 0; i < response.topics[topicId].links.length; i++) {
+        console.log(response.topics._id);
+        console.log(response.topics[topicId].links[i].url);
+        linksArray[i] = ("<li><a href='" + response.topics[topicId].links[i].url + "'>" + response.topics[topicId].links[i].url + '</a>');
+      }
+      var linksUL = $("<ul>").append(linksArray);
+      $('#'+topicId).append(linksUL);
+      $(this).off('click')
     })
-    }).fail(function(response){
-      console.log("Ajax get request failed.");
-    });
+  }).fail(function(response){
+    console.log("Ajax get request failed.");
   });
+});
