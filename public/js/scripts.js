cedec6ac5412db4c97207c35fe01307440b87e1a
Alexandra <akscreven@gmail.com>
2015-12-09 11:38:57 -0500

    appending to page

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,7 +6,8 @@ $(document).ready(function() {
       dataType: 'json',
       url: "http://localhost:4000/topics"
     }).done(function(response) {
-      console.log(response);
+      console.log(response.topics[0].name);
+      $('.postlinks').append(response.topics[0].name);
     }).fail(function(response){
       console.log("Ajax get request failed.");
     });
