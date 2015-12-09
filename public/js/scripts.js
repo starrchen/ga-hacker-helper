342b5cf3a6cf8bee7065ba9e754841bba5096d1a
Alexandra <akscreven@gmail.com>
2015-12-09 11:49:03 -0500

    working on rendering topics

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,7 +7,14 @@ $(document).ready(function() {
       url: "http://localhost:4000/topics"
     }).done(function(response) {
       console.log(response.topics[0].name);
-      $('.postlinks').append(response.topics[0].name);
+      var links = function() {
+        for(i = 0; i < response.topics[0].links.length; i++) {
+        response.topics[0].links[i]
+      }
+    }
+      console.log(response)
+      console.log(links)
+      $('.postlinks').append(links);
     }).fail(function(response){
       console.log("Ajax get request failed.");
     });
