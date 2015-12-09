52e1be6c9a660005b083434421eafb9c76b12209
Alice Frumin <alifrumin@yahoo.com>
2015-12-09 16:41:24 -0500

    ajax practice

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,31 +1,32 @@
-
 $(document).ready(function() {
-  $(".linktitle").on("click", function() {
-    console.log("clicked");
-    var topicId = $(this).attr('id')
-    console.log(topicId)
-   var topicInfo = $("<div>").attr("id",topicId);
-   console.log(topicInfo)
-    $.ajax({
-      type: 'GET',
-      dataType: 'json',
-      url: "http://localhost:4000/topics"
-    }).done(function(response) {
-      console.log(topicId);
-      var links = function() {
-        var linksArray = []
-        for(i = 0; i < response.topics[0].links.length; i++) {
-          console.log(response.topics._id);
-          console.log(response.topics[0].links[i].url);
-          linksArray[i] = ("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
-          
-        }
-        return linksArray
-      };
-      links()
-      $('#'+topicId).append(links);
-    }).fail(function(response){
-      console.log("Ajax get request failed.");
-    });
-  });
+ var url = "http://localhost:4000/topics.json"
+ var request = $.ajax({
+   url: url,
+   type: "GET",
+   dataType: "json"
+ }).done(function(response){
+
+   var allTopics = [];
+
+   for (var i = 0; i < response.topics.length; i++){
+     console.log(response.topics[i].name);
+    //  $(".all-topics").append("<div class='topic-" +  i + "'><h3>" + response.topics[i].name + "</h3><div>"+ response.topics[i].links +"</div></div>");
+     var topic = response.topics[i]
+     var topicdiv = $('<div/>', {class: "topic-" + i})
+     topicdiv.text(topic.name)
+     console.log("topicdiv", topicdiv)
+    //  links_div = $("div")
+    //  //for each link in topic.links
+    //    links_div.append(link)
+    // }
+    // topicdiv.append(links_div)
+     $(".all-topics").append(topicdiv)
+   }
+
+ }).fail(function(response){
+   console.log("Topics fetch fail");
+
+ }).always(function(response){
+   console.log("Beep boop!");
+ })
 });
