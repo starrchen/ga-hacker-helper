cf3f6fde0064e1abb3da440ac01ad3bfd4242b1c
Starr Z. Chen <chenstarr@gmail.com>
2015-12-09 14:55:19 -0500

    Attempting to merge in Alex's click-to-show-links functionality

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   var url = "http://localhost:4000/topics.json"
   var request = $.ajax({
     url: url,
@@ -21,27 +22,33 @@ $(document).ready(function() {
   })
 });
 
-  // $(".linktitle").on("click", function() {
-  //   console.log("clicked");
-  //   $.ajax({
-  //     type: 'GET',
-  //     dataType: 'json',
-  //     url: "http://localhost:4000/topics"
-  //   }).done(function(response) {
-  //     console.log(response);
-      // console.log(response.topics[0].name);
-      // var links = function() {
-      //   for(i = 0; i < response.topics[0].links.length; i++) {
-      //     console.log(response.topics[0].links[i].url)
-      //     $('.postlinks').append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
-      //
-      //   }
-      // }
-      // $('.postlinks').append(links);
-//
-//
-//     }).fail(function(response){
-//       console.log("Ajax get request failed.");
-//     });
-//   });
-// });
+function showLinks(topics){
+  for (var i = 0; i < topics.length; i++){
+    $(".topic-" + i).on("click", function() {
+      console.log("clicked");
+      var topicId = $(this).attr('id');
+      console.log(topicId);
+      var topicInfo = $("<div>").attr("id",topicId);
+      console.log(topicInfo);
+      $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: "http://localhost:4000/topics"
+      }).done(function(response) {
+        console.log(topicId);
+        var links = function() {
+          for(i = 0; i < response.topics[0].links.length; i++) {
+            console.log(topicInfo);
+            console.log(response.topics[0].links[i].url);
+            $('#'+topicId).append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
+          }
+        };
+        $('.postlinks').append(links);
+      }).fail(function(response){
+        console.log("Ajax get request failed.");
+      });
+    });
+  }
+}
+
+showLinks();
