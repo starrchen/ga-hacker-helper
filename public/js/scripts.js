9155b80ccbac52b3430b626fb05b280c3eb7fab2
Alexandra <akscreven@gmail.com>
2015-12-09 14:35:06 -0500

    only attaches to topic clicked

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,18 +1,22 @@
+
 $(document).ready(function() {
   $(".linktitle").on("click", function() {
     console.log("clicked");
-    console.log($(this).attr('id'))
+    var topicId = $(this).attr('id')
+    console.log(topicId)
+   var topicInfo = $("<div>").attr("id",topicId);
+   console.log(topicInfo)
     $.ajax({
       type: 'GET',
       dataType: 'json',
       url: "http://localhost:4000/topics"
     }).done(function(response) {
-      console.log(response.topics[0].name);
-      console.log(response.topics[0]._id)
+      console.log(topicId);
       var links = function() {
         for(i = 0; i < response.topics[0].links.length; i++) {
+          console.log(topicInfo);
           console.log(response.topics[0].links[i].url);
-          $('.postlinks').append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
+          $('#'+topicId).append("<li><a href='" + response.topics[0].links[i].url + "'>" + response.topics[0].links[i].url + '</a>');
         }
       };
       $('.postlinks').append(links);
