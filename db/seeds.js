80c2165f9a4f564355cfc39d3412ab6b918a79c2
Starr Z. Chen <chenstarr@gmail.com>
2015-12-10 14:26:29 -0500

    Added some seeds (Atom) for testing

--- db/seeds.js
+++ db/seeds.js
@@ -22,12 +22,6 @@ var jobs = new TopicModel({
   description: "Interview tips, resume building resources, open positions, and more",
   imageUrl: "images/hireme.png"
 });
-//
-// var lol = new TopicModel({
-//   name: "For the Lulz",
-//   description: "",
-//   imageUrl: "images/lol.png"
-// });
 
 var powermode = new LinkModel({
     title: "Power Mode",
@@ -35,21 +29,20 @@ var powermode = new LinkModel({
     url: "https://atom.io/packages/activate-power-mode"
 });
 
-var techQs = ({
+var techQs = new LinkModel({
   title: "Technical Questions",
   summary: "List of questions/question topics a Redditor had at tech interviews",
   url: "https://www.reddit.com/r/webdev/comments/3f7q3q/been_interviewing_with_a_lot_of_tech_startups_as/"
 });
 
-var pokedata = ({
-  title: "Pokemon or Big Data?",
-  summary: "A game that tests if you know the difference between Pokemon names and Big Data terms",
-  url: "http://pixelastic.github.io/pokemonorbigdata/"
-});
+// var pokedata = ({
+//   title: "Pokemon or Big Data?",
+//   summary: "A game that tests if you know the difference between Pokemon names and Big Data terms",
+//   url: "http://pixelastic.github.io/pokemonorbigdata/"
+// });
 
-// var topics = [atom, jobs, lol];
 var topics = [atom, jobs];
-var links = [powermode, techQs, pokedata];
+var links = [powermode, techQs];
 
 for(var i = 0; i < topics.length; i++){
   topics[i].links.push(links[i]);
@@ -60,4 +53,42 @@ for(var i = 0; i < topics.length; i++){
       console.log("topic was saved");
     }
   });
-}
+};
+
+// more seeds!
+var mergeConflicts = new LinkModel({
+  title: "Merge Conflicts",
+  summary: "See and resolve merge conflicts from within the Atom text editor",
+  url: "https://atom.io/packages/merge-conflicts"
+});
+
+var toDoShow = new LinkModel({
+  title: "To Do Show",
+  summary: "Stay on track by tracking action items and notes within your code",
+  url: "https://atom.io/packages/todo-show"
+});
+
+var railsSnippets = new LinkModel({
+  title: "Rails Snippets",
+  summary: "Easily add rails snippets (aka clown hats) to your Ruby on Rails projects",
+  url: "https://atom.io/packages/rails-snippets"
+});
+
+var rubyBlock = new LinkModel({
+  title: "Ruby Block",
+  summary: "Find the matching Ruby block for keywords end, elsif, else, when, rescue, and ensure",
+  url: "https://atom.io/packages/ruby-block"
+})
+
+var moreAtomLinks = [mergeConflicts, toDoShow, railsSnippets, rubyBlock];
+
+for(var j = 0; i < moreAtomLinks.length; i++){
+  atom.links.push(moreAtomLinks[i]);
+  atom.save(function(err){
+    if(err){
+      console.log(err);
+    } else {
+      console.log("Atom links saved!");
+    }
+  })
+};
